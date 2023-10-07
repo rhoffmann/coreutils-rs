bfb88f08108a9bd165d89c1b6c06174f9ca28900
Richard Hoffmann <rhoffmann@fastmail.com>
2023-10-07 22:01:43 +0200

    add clap and command line options

--- echo-rs/src/main.rs
+++ echo-rs/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echo-rs")
+        .version("0.1")
+        .author("Richard Hoffmann <rhoffmann@fastmail.com>")
+        .about("Echo for Rust")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("no_newline")
+                .short("n")
+                .help("Do not output the trailing newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:#?}", matches)
 }
