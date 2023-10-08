9ec41bdbef6dac1d5daef1d836d5c6cf204f9132
Richard Hoffmann <rhoffmann@fastmail.com>
2023-10-08 14:08:51 +0200

    add logic

--- echo-rs/src/main.rs
+++ echo-rs/src/main.rs
@@ -20,5 +20,10 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", matches)
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("no_newline");
+
+    let ending = if omit_newline { "" } else { "\n" };
+
+    println!("{}{}", text.join(" "), ending);
 }
