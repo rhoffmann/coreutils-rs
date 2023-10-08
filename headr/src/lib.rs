00304c322eb84cf6b8d05bdaf3228a79b558c6ae
Richard Hoffmann <rhoffmann@fastmail.com>
2023-10-08 21:49:03 +0200

    wip argument parsing hin headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,94 @@
+use std::error::Error;
+
+use clap::{App, Arg};
+
+type ResultType<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+pub fn get_args() -> ResultType<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Richard Hoffmann <rhoffmann@fastmail.com>")
+        .about("head in rust")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .multiple(true)
+                .help("Input file(s)")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number lines")
+                .default_value("10")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .conflicts_with("lines")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+    })
+}
+
+pub fn run(config: Config) -> ResultType<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> ResultType<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)), // or Err(val.into())
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("-3");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "-3".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
