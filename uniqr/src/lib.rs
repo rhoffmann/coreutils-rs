f6fb05c62893f4ae22426773c247816b0bae9c98
Richard Hoffmann <rhoffmann@fastmail.com>
2023-10-15 15:11:49 +0200

    finish uniqr

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
 };
 
 use clap::{App, Arg};
@@ -51,7 +51,46 @@ pub fn get_args() -> GenericResult<Config> {
 }
 
 pub fn run(config: Config) -> GenericResult<()> {
-    println!("{:?}", config);
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    let mut line = String::new();
+    let mut previous = String::new();
+
+    let mut count: u64 = 0;
+
+    let mut print = |count: u64, text: &str| -> GenericResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous);
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
+        line.clear();
+    }
+
+    print(count, &previous);
+
     Ok(())
 }
 
