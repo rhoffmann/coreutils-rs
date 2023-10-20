e9947fa1acfbfdf2954d6d621ef033664b145805
Richard Hoffmann <rhoffmann@fastmail.com>
2023-10-20 20:12:05 +0200

    handle names and types args

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -34,29 +34,56 @@ pub fn get_args() -> GenericResult<Config> {
                 .help("Search paths"),
         )
         .arg(
-            Arg::with_name("name")
+            Arg::with_name("names")
                 .value_name("NAME")
                 .short("n")
                 .long("name")
+                .takes_value(true)
                 .multiple(true)
                 .help("Name"), // .takes_value(false),
         )
         .arg(
-            Arg::with_name("type")
+            Arg::with_name("types")
                 .value_name("TYPE")
                 .short("t")
                 .long("type")
+                .takes_value(true)
                 .multiple(true)
                 .possible_values(&["f", "d", "l"])
                 .help("Entry type"),
         )
         .get_matches();
 
+    let names = matches
+        .values_of_lossy("names")
+        .map(|values| {
+            values
+                .into_iter()
+                .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .transpose()?
+        .unwrap_or_default();
+
+    let entry_types = matches
+        .values_of_lossy("types")
+        .map(|values| {
+            values
+                .iter()
+                .map(|val| match val.as_str() {
+                    "d" => Directory,
+                    "f" => File,
+                    "l" => Link,
+                    _ => unreachable!("Invalid type"),
+                })
+                .collect()
+        })
+        .unwrap_or_default();
+
     Ok(Config {
-        // paths: matches.value_of_lossy("paths").unwrap().to_string(),
         paths: vec![],
-        names: vec![],
-        entry_types: vec![],
+        names,
+        entry_types,
     })
 }
 
